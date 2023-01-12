9c6a654be0d626f172c2ca9d0f2ed92285db8b8f
Capaldi <4sentepoid4@gmail.com>
2023-01-13 02:10:08 +0300

    Add gallery main page

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .utils import Router
+from .models import Image
 
 
 route = Router()
@@ -8,4 +9,6 @@ route = Router()
 
 @route('', name='index')
 def index(request):
-    return render(request, 'index.html')
+    images = Image.objects.all().order_by('-created_at')
+
+    return render(request, 'index.html', {'images': images})
