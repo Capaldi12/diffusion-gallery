90e877a3b652ded8269f874af5c068cacc964d9d
Capaldi <4sentepoid4@gmail.com>
2023-01-13 21:22:11 +0300

    Implement prompt creation page

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .utils import Router
-from .models import Image
-
+from .models import Image, Prompt, Task
 
 route = Router()
 
@@ -12,3 +13,28 @@ def index(request):
     images = Image.objects.all().order_by('-created_at')
 
     return render(request, 'index.html', {'images': images})
+
+
+@route('generate', name='generate')
+class PromptCreateView(CreateView):
+    model = Prompt
+    fields = ['name', 'text', 'model', 'width', 'height', 'steps', 'seed']
+    success_url = reverse_lazy('index')
+    template_name = 'prompt_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # Don't display `generate` button
+        context['no_button'] = True
+
+        return context
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # Create task for the prompt
+        # TODO: multiple tasks per prompt
+        Task.objects.create(prompt=self.object)
+
+        return response
