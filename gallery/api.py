b684e232e097b4f817ffcf06cb5b38dbf272f7f3
Capaldi12 <4sentepoid4@gmail.com>
2024-01-24 14:21:23 +0300

    Add model list endpoint

--- gallery/api.py
+++ gallery/api.py
@@ -40,6 +40,7 @@ def api_root(request):
         links={
             'prompts': reverse('prompt_list'),
             'images': reverse('image_list'),
+            'models': reverse('model_list'),
         },
     )
 
@@ -47,7 +48,6 @@ def api_root(request):
 @route('images/', name='image_list')
 @route('images/<int:image_id>/', name='image_details')
 @route('prompts/<int:prompt_id>/images/', name='prompt_images')
-@route('prompts/<int:prompt_id>/images/<int:image_id>/', name='prompt_image')
 class ImageView(View):
     def get(self, request, prompt_id=None, image_id=None):
         if image_id:
@@ -66,13 +66,7 @@ class ImageView(View):
                 {
                     'name': image.name,
                     'src': image.image.url,
-                    'url': reverse(
-                        'prompt_image',
-                        args=[prompt_id, image.id],
-                    ) if prompt_id else reverse(
-                        'image_details',
-                        args=[image.id],
-                    ),
+                    'url': reverse('image_details', args=[image.id]),
                 }
                 for image in data
             ],
@@ -97,16 +91,20 @@ class ImageView(View):
 
 @route('prompts/', name='prompt_list')
 @route('prompts/<int:prompt_id>/', name='prompt_details')
+@route('models/<int:model_id>/prompts/', name='model_prompts')
 @method_decorator(csrf_exempt, name='dispatch')  # No CSRF protection
 class PromptView(View):
-    def get(self, request, prompt_id=None):
+    def get(self, request, model_id=None, prompt_id=None):
         if prompt_id:
             return self.get_prompt(prompt_id)
         else:
-            return self.get_list()
+            return self.get_list(model_id)
 
-    def get_list(self):
-        data = Prompt.objects.all()
+    def get_list(self, model_id=None):
+        if model_id:
+            data = Prompt.objects.filter(model_id=model_id)
+        else:
+            data = Prompt.objects.all()
 
         return success({
             'prompts': [
@@ -196,3 +194,35 @@ def generate(request, prompt_id, count=1):
     prompt.create_task(count)
 
     return success()
+
+
+@route('models/', name='model_list')
+@route('models/<int:model_id>/', name='model_details')
+def models(request, model_id=None):
+    if model_id:
+        model = DiffusionModel.objects.filter(id=model_id).first()
+
+        if model is None:
+            return fail(404, 'Model not found')
+
+        return success({
+            'model': {
+                'name': model.name,
+                'available': model.available,
+                'download_url': model.url,
+                'prompts': reverse('model_prompts', args=[model.id])
+            }
+        })
+
+    models_ = DiffusionModel.objects.all()
+
+    return success({
+        'models': [
+            {
+                'name': model.name,
+                'available': model.available,
+                'url': reverse('model_details', args=[model.id])
+            }
+            for model in models_
+        ],
+    })
